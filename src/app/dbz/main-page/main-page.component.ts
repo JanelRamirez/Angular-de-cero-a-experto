1f466b835b9782b59b4c4c15d0155b40695685b3
JanelRamirez <janel.2222107@gmail.com>
2022-04-27 16:02:07 -0400

    @input interfaces modules

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Personaje {
+  nombre: string;
+  poder : number;
+}
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent  {
+
+  personajes : Personaje[] = [
+    {
+      nombre: 'Vegueta',
+      poder: 15000
+    },
+    {
+      nombre: 'Goku',
+      poder: 17500
+    }
+  ]
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+ 
+ agregar(){
+    if(this.nuevo.nombre.trim().length === 0){
+      return;
+    }
+    this.personajes.push(this.nuevo);  
+    this.nuevo = { nombre: '', poder:0}
+ }
+
+}
